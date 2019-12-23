5153fb4cf84a8331c988f838f037e82eae654089
DrecosS <snauriz_a@hotmail.com>
2019-12-23 11:17:35 -0300

    Acrescentada a mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,11 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		
-		sc.close();
+		System.out.println("Bom dia!");
 	}
 
 }
